0dbdd0cfa532a8c5f988a25ad3a5eefc0a31d0df
DeodatoBastos <deodatobasto@gmail.com>
2024-04-05 09:49:18 -0300

    fix utils functions

--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,35 @@
 #include "utils.h"
 
-bool is_in_cmd(const char *str) { return !strcmp(str, IN_CMD); }
+bool is_in_cmd(const char *str) {
+    if (str == NULL)
+        return false;
 
-bool is_out_cmd(const char *str) { return !strcmp(str, OUT_CMD); }
+    return !strcmp(str, IN_CMD);
+}
 
-bool is_pipe_cmd(const char *str) { return !strcmp(str, PIPE_CMD); }
+bool is_out_cmd(const char *str) {
+    if (str == NULL)
+        return false;
+    return !strcmp(str, OUT_CMD);
+}
+
+bool is_pipe_cmd(const char *str) {
+    if (str == NULL)
+        return false;
+    return !strcmp(str, PIPE_CMD);
+}
 
 void execute(char **argv, char *cmd, char *path, char *const *envp) {
+    if (argv == NULL || cmd == NULL || envp == NULL)
+        exit(EXIT_FAILURE);
+
+    if (path == NULL) {
+        if (execve(cmd, argv, envp) == ERROR_CODE) {
+            fprintf(stderr, "msh: command not found: %s\n", cmd);
+        }
+        return;
+    }
+
     char *pre;
     for (pre = strtok(path, PATH_DELIMITER); pre != NULL; pre = strtok(NULL, PATH_DELIMITER)) {
         char fullpath[strlen(pre) + strlen(cmd) + 1];
@@ -49,4 +72,6 @@ void file_logging(FILE *file, const char severity, const char *info) {
     fprintf(file, "%s: %d-%02d-%02d %02d:%02d:%02d -- ", msg, tm.tm_year + 1900, tm.tm_mon + 1,
             tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
     fprintf(file, "executing: %s\n", info);
+
+    free(msg);
 }
